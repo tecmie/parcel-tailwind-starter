dba9c553f9e96ed960e05e73304e07fd4c89e57e
Andrew Miracle <me@andrewmiracle.com>
2024-04-08 01:32:17 +0000

    setup defaults for tailwind config and sample html page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,6 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
   ],
   theme: {
-      fontSize: {
-        sm: '1.45rem',
-        base: '1.852rem',
-        xl: '2rem',
-        '2xl': '3.3rem',
-        '3xl': '4.3rem',
-        '4xl': '5.415rem',
-        '5xl': '7.52rem',
-        '6xl': '9.52rem',
-      },
     extend: {
       colors: {},
     },
