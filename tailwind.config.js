8e5db4bfa782b29dacff4490d84605eafbf74878
Andrew Miracle <me@andrewmiracle.com>
2024-04-08 10:12:25 +0000

    install the tailwindcss animate plugin and implement the related classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,hbs,js,ts,jsx,tsx}"],
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animatecss"), require("@tailwindcss/aspect-ratio")],
   theme: {
+    animatedSettings: {
+      animatedSpeed: 3000,
+      heartBeatSpeed: 500,
+      hingeSpeed: 2000,
+      bounceInSpeed: 750,
+      bounceOutSpeed: 750,
+      animationDelaySpeed: 500,
+      classes: ["bounce", "fadeIn", "bounceIn", "zoomIn", "zoomOut", "fadeInDown", "fadeInUp", "fadeOut", "zoomInUp", "zoomOut", "slideInDown", "slideInUp", "slideOutDown", "slideOutUp", "heartBeat"],
+    },
     extend: {
       colors: {},
     },
