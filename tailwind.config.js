f34e21ac9bf99c510411dd5797cd3c5ebc0f396e
Andrew Miracle <me@andrewmiracle.com>
2024-04-08 18:25:20 +0000

    prettier format and standardize animation on pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ],
+  safelist: ["!duration-[0ms]", "!delay-[0ms]", 'html.js :where([class*="taos:"]:not(.taos-init))'],
   content: {
     relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
+    transform: (content) => content.replace(/taos:/g, ""),
     files: ["./src/**/*.{html,hbs,js,ts,jsx,tsx}"],
   },
   // content: ["./src/**/*.{html,hbs,js,ts,jsx,tsx}"],
-  plugins: [require("@tailwindcss/typography"),  require('taos/plugin'), require("@tailwindcss/aspect-ratio")],
+  plugins: [require("@tailwindcss/typography"), require("taos/plugin"), require("@tailwindcss/aspect-ratio")],
   theme: {
     extend: {
       colors: {},
